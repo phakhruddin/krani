158122126cfe7cd340798431954c6e668bde921d
Aina Phakhruddin <ainaphakhruddin@Ainas-MacBook-Air.local>
2015-03-29 09:11:12 -0500

    camera and video

--- app/controllers/enterjobdetail.js
+++ app/controllers/enterjobdetail.js
@@ -8,3 +8,77 @@ exports.openMainWindow = function(_tab) {
 function closeWin(e) {
 	console.log("e is: "+JSON.stringify(e));
 }
+
+function UploadPhotoToServer(e){
+	console.log("Upload photo to the server.");
+}
+
+function uploadFile(e){
+	console.log("JSON stringify e" +JSON.stringify(e));
+       Titanium.Media.openPhotoGallery({
+           success:function(event)
+           {             
+               Ti.API.debug('Our type was: '+event.mediaType);
+               if(event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO)
+               {
+                   UploadPhotoToServer(event.media);
+               }
+           },
+           cancel:function()
+           {   
+           },
+           error:function(err)
+           {
+               Ti.API.error(err);
+           },
+           mediaTypes:[Ti.Media.MEDIA_TYPE_PHOTO]
+       });
+   }
+   
+   var win = Titanium.UI.createWindow({
+					title:"Media",
+					backgroundColor: "black"
+				});
+
+function takePic(e){ 
+	console.log("JSON stringify e" +JSON.stringify(e));
+	Titanium.Media.showCamera({
+		success:function(e){
+			if(e.mediaType === Titanium.Media.MEDIA_TYPE_PHOTO){
+				var ImageView = Titanium.UI.createImageView({
+					image:e.media,
+					width:288,
+					height:215,
+					top:12,
+					zIndex:1
+				});
+				win.add(ImageView);
+			} else if (e.mediaType === Titanium.Media.MEDIA_TYPE_VIDEO){
+				var w = Titanium.UI.createWindow({
+					title:"Job Video",
+					backgroundColor: "black"
+				});
+				
+				var videoPlayer = Titanium.Media.createVideoPlayer({
+					media: e.media				
+				});
+				w.add(videoPlayer);
+				videoPlayer.addEventListener("complete",function(e){
+					w.remove(videoPlayer);
+					videoPlayer = null ;
+					w.close();
+				});		
+				
+			}
+			
+		}, error:function(e){
+			console.log("error taking Pic: ");
+		}, cancel:function(e){
+			console.log("cancel taking Pic: ");
+		},
+		allowEditing:true,
+		saveToPhotoGallery:true,
+		mediaTypes:[Titanium.Media.MEDIA_TYPE_PHOTO,Titanium.Media.MEDIA_TYPE_VIDEO],
+		videoQuality:Titanium.Media.QUALITY_HIGH
+	});
+	}
