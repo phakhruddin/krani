131b4230c184cad8a7e35feec45fc457f1d3f972
Abdullah <dabdullah@SDGL13dae2506.local>
2015-05-12 05:07:10 -0500

    color coded status

--- app/controllers/projectdetail.js
+++ app/controllers/projectdetail.js
@@ -27,7 +27,7 @@ var city = data[7];
 var state = data[8];
 var country = data[9];
 var status = data[10];
-var notes = data[11];
+var notesraw = data[11];
 var percentcomplete = data[12];
 var nextappt = data[13];
 var datedue = data[14];
@@ -46,7 +46,6 @@ someDummy.set('fulladdress',+address+' , '+city+' , '+state);
 someDummy.set('country', country);
 someDummy.set('firstname', firstname);
 someDummy.set('lastname', lastname);
-someDummy.set('notes', notes);
 someDummy.set('percentcomplete', percentcomplete);
 someDummy.set('nextappt', nextappt);
 someDummy.set('datedue', datedue);
@@ -72,11 +71,25 @@ var dateduelabel = Ti.UI.createLabel ({
         },
         text : datedue
 });
+
+if (percentcomplete == "0") {
+	var status = "NOT STARTED";
+	var color = "red";
+	var left = 153;
+} else if ( percentcomplete == "100" ) {
+	var status = "COMPLETED";
+	var color = "green";
+	var left = 164;
+} else {
+	var status = "PENDING";
+	var color = "orange";
+	var left = 167;
+}
         
 var statuslabel = Ti.UI.createLabel ({
-        color : (status == "done")?"green":"orange",
-        left  : "20",
-        textAlign : "Ti.UI.TEXT_ALIGNMENT_LEFT",
+        color : color,
+        left  : left,
+        textAlign : "Ti.UI.TEXT_ALIGNMENT_CENTER",
         top : "10",
         font:{
         	fontSize:18
@@ -85,14 +98,14 @@ var statuslabel = Ti.UI.createLabel ({
 });
 
 var percentcompletelabel = Ti.UI.createLabel ({
-        color : (percentcomplete == "100")?"green":"orange",
-        left  : "200",
-        textAlign : "Ti.UI.TEXT_ALIGNMENT_LEFT",
-        top : "10",
+        color : color,
+        left  : "135",
+        textAlign : "Ti.UI.TEXT_ALIGNMENT_CENTER",
+        top : "32",
         font:{
-        	fontSize:18
+        	fontSize:12
         },
-        text : percentcomplete+"%"
+        text : 'Percentage Completion: '+percentcomplete+"%"
 });
 
 $.status_row.add(statuslabel);
@@ -146,33 +159,23 @@ $.addbutton.setTitleid(args);
 $.addbutton.addEventListener("click", function(e){
 	console.log("projectdetail.js::JSON stringify e on addHandler: "+JSON.stringify(e));
 	console.log("projectdetail.js::JSON stringify e on addHandler args: "+JSON.stringify(args));
-	/*
-	var item = "joblog";
-    	//Get joblog specific to project - START
-	var projectid = args.title.split(':')[15];
-	var firstname = args.title.split(':')[1];
-	var lastname = args.title.split(':')[2];
-	var filename = 'project_'+projectid+'_'+firstname+'_'+lastname;
-	var parentid = Titanium.App.Properties.getString('parentid');
-	console.log("projectdetail.js::need to check if parent/filename exist: "+parentid+'/'+filename);
-	fileExist(filename,parentid);
-	var item = "joblog";
-	//var sid = Titanium.App.Properties.getString(item,"none");
-	// Get joblog specific to project - END
-	//var sid = Titanium.App.Properties.getString('sid');
-	// sid check
-	var sidmatch = matchjoblogsidfromDB(filename);
-	var sid = sidmatch;
-	console.log("projectdetail.js::sidmatch: sid "+sidmatch+' : '+sid);
-	Alloy.Globals.getPrivateData(sid,item);
-	console.log("projectdetail.js:: Alloy.Collections.joblog.fetch()");
-	Alloy.Collections.joblog.fetch();*/
 	var sid = matchjoblogsidfromDB(filename);
 	var tabViewOneController = Alloy.createController("enterjobdetail",{
 			title: args,
 			sid: sid
 		});
 	tabViewOneController.openMainWindow($.tab_projectdetail);	
+});
+
+$.joblog_button.addEventListener("click", function(e){
+	console.log("projectdetail.js::JSON stringify e on jobLog: "+JSON.stringify(e));
+	console.log("projectdetail.js::JSON stringify e on jobLog args: "+JSON.stringify(args));
+	var sid = matchjoblogsidfromDB(filename);
+	var tabViewOneController = Alloy.createController("enterjobdetail",{
+			title: args,
+			sid: sid
+	});
+	tabViewOneController.openMainWindow($.tab_projectdetail);		
 });
 
 function addHandler(e,args){
@@ -244,27 +247,6 @@ function getParentFolder(args) {
 	};
 	xhr.open("GET", 'https://www.googleapis.com/drive/v2/files/'+sid+'/parents');
 	xhr.setRequestHeader("Content-type", "application/json");
-    xhr.setRequestHeader("Authorization", 'Bearer '+ googleAuthSheet.getAccessToken());
-	xhr.send();
-};
-
-function getSSKey() {
-	var xhr = Ti.Network.createHTTPClient({
-	    onload: function(e) {
-	    try {
-	    		var xml = Titanium.XML.parseString(this.responseText);
-	    		Ti.API.info("getSSKey:: response is: "+this.responseText);
-	    		Ti.API.info("getSSKey:: xml response is: "+xml);
-	    	} catch(e){
-				Ti.API.info("cathing e: "+JSON.stringify(e));
-			}
-		}
-		});
-	xhr.onerror = function(e){
-		alert("Unable to connect to the cloud.");
-	};
-	xhr.open("GET", 'https://spreadsheets.google.com/feeds/spreadsheets/private/full');
-	xhr.setRequestHeader("Content-type", "application/atom+xml");
     xhr.setRequestHeader("Authorization", 'Bearer '+ googleAuthSheet.getAccessToken());
 	xhr.send();
 };
@@ -341,21 +323,6 @@ function getSSCell(sid,rowno,colno,value) {
     xhr.setRequestHeader("Authorization", 'Bearer '+ googleAuthSheet.getAccessToken());
 	xhr.send();
 };
-
-$.GetSSCell.addEventListener("click", function(e){
-	var sid = Titanium.App.Properties.getString('sid');
-	//var sid = '1gecYbrWtzS3Zr5d6kpzmYBAxe4UXncKylKfSMREiDtM';
-	for (i=1;i<17;i++){
-		var value = "col"+i;
-		getSSCell(sid,1,i,value);
-	}
-	getSSCell(sid,2,1,"Date");
-	getSSCell(sid,2,2,"Notes");
-	var date = new Date();
-	getSSCell(sid,3,1,date);
-	getSSCell(sid,3,2,"Please enter work logs.");
-	//getSSCell(sid,1,1,'col1');
-});
 
 getParentFolder();
 
@@ -530,4 +497,58 @@ function getjoblogSID(thefilename){
     	}
     }   
 }	
+
+/// processing array in notes
+if (notesraw != "none") {
+	var notesstring = notesraw.replace(/cOlOn/g,':');   // replacing all cOlOn to ':'
+	var notes = JSON.parse(notesstring);
+	var descr = notes[0].descr;
+	var descrtitlelabel = Ti.UI.createLabel ({
+		left  : "20",
+		textAlign : "Ti.UI.TEXT_ALIGNMENT_LEFT",
+		top : "10",
+		font:{
+			fontSize:14
+		},
+		text : 'Description: '
+	});
+	var descrbodylabel = Ti.UI.createLabel ({
+		color : "#333",
+		left  : "120",
+		textAlign : "Ti.UI.TEXT_ALIGNMENT_LEFT",
+		top : "10",
+		font:{
+			fontSize:12
+		},
+		text : descr
+	});
+	$.jobdescr_row.add(descrtitlelabel);
+	$.jobdescr_row.add(descrbodylabel);
+	var itemtitlelabel = Ti.UI.createLabel ({
+		left  : "20",
+		textAlign : "Ti.UI.TEXT_ALIGNMENT_LEFT",
+		top : "4",
+		font:{
+			fontSize:14
+		},
+		text : 'List Item :'
+	});
+	if ( notes.length > 1) {$.jobitem_row.add(itemtitlelabel); }
+	var topvalue = 22;
+	for (i=1;i<notes.length;i++){
+		var itembodylabel = Ti.UI.createLabel ({
+			color : "#333",
+			left  : "20",
+			textAlign : "Ti.UI.TEXT_ALIGNMENT_LEFT",
+			top : topvalue,
+			font:{
+				fontSize:12
+			},
+			text : i+' :    '+notes[i].lineitem
+		});		
+		$.jobitem_row.add(itembodylabel);
+		var topvalue = topvalue + 18;
+	}
+}
+
 
