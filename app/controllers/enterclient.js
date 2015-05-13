ea6a9e10ec3aea67dd3b2172b4be8c9bb2b09a21
Abdullah <dabdullah@SDGL13dae2506.local>
2015-05-13 06:59:08 -0500

    enterclient new format

--- app/controllers/enterclient.js
+++ app/controllers/enterclient.js
@@ -1,150 +1,226 @@
+var args = arguments[0] || {};
 exports.openMainWindow = function(_tab) {
   _tab.open($.enterclient_window);
   Ti.API.info("This is child widow schedule.js" +JSON.stringify(_tab));
-    //var result = Alloy.Globals.checkNetwork('1gnkP116nsTVxtrw6d_mXVdOiesQEPH7LVUIyHUfx9EE');
-   Alloy.Globals.checkNetworkAndGoogleAuthorized('1gnkP116nsTVxtrw6d_mXVdOiesQEPH7LVUIyHUfx9EE');
+    Alloy.Globals.checkNetworkAndGoogleAuthorized('1gnkP116nsTVxtrw6d_mXVdOiesQEPH7LVUIyHUfx9EE');
+	googleAuth.authorize();
+	
+};
+
+function addRows(){
+ console.log("JSON stringify e : " +JSON.stringify(e));
+// Defining new row
+var newRow = Ti.UI.createTableViewRow({
+title : 'Row ' + ($.enterclient_table.data[0].rowCount + 1)
+});
+ 
+// Adding row to the table view
+$.enterclient_table.appendRow(newRow);
+}
+
+
+var count = 3; //row where line item is entered
+Titanium.App.Properties.setInt('count',count);
+
+function addItem(e,itemTextField){
+	var count = Titanium.App.Properties.getInt('count',3);
+    console.log("count :" +count);
+	//log
+	console.log("JSON stringify e : " +JSON.stringify(e));
+	Ti.API.info("data length " +$.enterclient_table.data.length);		
+	Ti.API.info("table data 0 "+$.enterclient_table.data[0]);
+	Ti.API.info("table row count : "+$.enterclient_table.data[0].rowCount);
+	console.log("JSON stringify table data 0 : " +JSON.stringify($.enterclient_table.data[0]));
+	
+	var itemval = count - 1;
+	// Defining new test field
+	var itemLabellist = Ti.UI.createLabel({
+		id:"tflabellist" , 
+		text:'Line item ' + itemval+' : ',
+		font : {
+			fontSize: '14',
+			fontweight : 'normal'
+		},
+		left: '20',
+		top: '10',
+		color: "#3B708A"
+		});
+	var itemTextField = Titanium.UI.createTextField({
+		id:"lineitem_tf",
+		borderColor : 'white', // border color
+    	width: Ti.UI.FILL,
+    	left:'120',
+    	top: '12',
+    	font: {fontSize: '14'}
+		});
+	var itemLabelqty = Ti.UI.createLabel({
+		id:"linetflabelqty" , 
+		text:'qty(opt) : ',
+		font : {
+			fontSize: '14',
+			fontweight : 'normal'
+		},
+		top: '30',
+		left: '20',
+		color: "#3B708A"
+		});
+	var itemTextFieldqty = Titanium.UI.createTextField({
+		id:"lineitemqty_tf",
+		borderColor : 'white', // border color
+    	top: '32',
+    	left: '90',
+    	width: '40',
+    	hintText: '1',
+    	width: '60',
+    	keyboardType: Ti.UI.KEYBOARD_NUMBER_PAD,
+    	returnKeyType : Ti.UI.RETURNKEY_DONE,
+    	font: {fontSize: '14'}
+		});
+	var itemLabelprice = Ti.UI.createLabel({
+		id:"linetflabelqty" , 
+		text:'price(opt) : ',
+		font : {
+			fontSize: '14',
+			fontweight : 'normal'
+		},
+		top: '30',
+		left: '180',
+		color: "#3B708A"
+		});
+	var itemTextFieldprice = Titanium.UI.createTextField({
+		id:"lineitemprice_tf",
+		borderColor : 'white', // border color
+       	top: '32',
+		left: '260',
+		hintText: '160',
+		width: '80',
+		keyboardType: Ti.UI.KEYBOARD_DECIMAL_PAD,
+		returnKeyType: Ti.UI.RETURNKEY_DONE, 
+		border: 1, 
+		width: 100,
+    	font: {fontSize: '14'}
+		});
+	var toolbarDone = Ti.UI.createButton({systemButton: Titanium.UI.iPhone.SystemButton.DONE});
+	var flexSpace = Titanium.UI.createButton({systemButton : Titanium.UI.iPhone.SystemButton.FLEXIBLE_SPACE});
+	toolbarDone.addEventListener('click', function(){
+		itemTextFieldprice.blur();
+	});
+	itemTextFieldprice.keyboardToolbarColor = '#80c342';
+	itemTextFieldprice.keyboardToolbar = [flexSpace, toolbarDone];
+	// Defining new row
+	var newRow = Ti.UI.createTableViewRow({
+		height: '50',
+		borderColor : 'white',
+		backgroundColor : "white"
+	});
+	newRow.add(itemLabellist);
+	newRow.add(itemTextField);
+	newRow.add(itemLabelqty);
+	newRow.add(itemTextFieldqty);
+	newRow.add(itemLabelprice);
+	newRow.add(itemTextFieldprice);
     
-    //Alloy.Globals.checkGoogleisAuthorized();
-	  
-  	$.save_clientlastname_button.addEventListener('click', function(_e) {
-    $.clientlastname_tf.blur();
-    var clientlastname = $.clientlastname_tf.value;
-    Ti.API.info("clientlastname entered is: "+clientlastname);
-    Titanium.App.Properties.setString('clientlastname',clientlastname);
-    Ti.API.info("clientlastname obtained is: "+Titanium.App.Properties.getString('clientlastname',"none"));
-    $.save_clientlastname_button.hide();
- });
-
-	$.clientlastname_tf.addEventListener("focus", function(){
- 	$.save_clientlastname_button.show();
- });
-
-	$.save_clientfirstname_button.addEventListener('click', function(_e) {
-    $.clientfirstname_tf.blur();
-    var clientfirstname = $.clientfirstname_tf.value;
-    Ti.API.info("clientfirstname entered is: "+clientfirstname);
-    Titanium.App.Properties.setString('clientfirstname',clientfirstname);
-    Ti.API.info("clientfirstname obtained is: "+Titanium.App.Properties.getString('clientfirstname',"none"));
-    $.save_clientfirstname_button.hide();
- });
-
-	$.clientfirstname_tf.addEventListener("focus", function(){
- 	$.save_clientfirstname_button.show();
- });
-
-	$.save_clientemail_button.addEventListener('click', function(_e) {
-    $.clientemail_tf.blur();
-    var clientemail = $.clientemail_tf.value;
-    Ti.API.info("clientemail entered is: "+clientemail);
-    Titanium.App.Properties.setString('clientemail',clientemail);
-    Ti.API.info("clientemail obtained is: "+Titanium.App.Properties.getString('clientemail',"none"));
-    $.save_clientemail_button.hide();
- });
-
-	$.clientemail_tf.addEventListener("focus", function(){
- 	$.save_clientemail_button.show();
- });
-
-	$.save_clientphone_button.addEventListener('click', function(_e) {
-    $.clientphone_tf.blur();
-    var clientphone = $.clientphone_tf.value;
-    Ti.API.info("clientphone entered is: "+clientphone);
-    Titanium.App.Properties.setString('clientphone',clientphone);
-    Ti.API.info("clientphone obtained is: "+Titanium.App.Properties.getString('clientphone',"none"));
-    $.save_clientphone_button.hide();
- });
-
-	$.clientphone_tf.addEventListener("focus", function(){
- 	$.save_clientphone_button.show();
- });
-
-	$.save_clientstreetaddress_button.addEventListener('click', function(_e) {
-    $.clientstreetaddress_tf.blur();
-    var clientstreetaddress = $.clientstreetaddress_tf.value;
-    Ti.API.info("clientstreetaddress entered is: "+clientstreetaddress);
-    Titanium.App.Properties.setString('clientstreetaddress',clientstreetaddress);
-    Ti.API.info("clientstreetaddress obtained is: "+Titanium.App.Properties.getString('clientstreetaddress',"none"));
-    $.save_clientstreetaddress_button.hide();
- });
-
-	$.clientstreetaddress_tf.addEventListener("focus", function(){
- 	$.save_clientstreetaddress_button.show();
- });
-
-	$.save_clientcity_button.addEventListener('click', function(_e) {
-    $.clientcity_tf.blur();
-    var clientcity = $.clientcity_tf.value;
-    Ti.API.info("clientcity entered is: "+clientcity);
-    Titanium.App.Properties.setString('clientcity',clientcity);
-    Ti.API.info("clientcity obtained is: "+Titanium.App.Properties.getString('clientcity',"none"));
-    $.save_clientcity_button.hide();
- });
-
-	$.clientcity_tf.addEventListener("focus", function(){
- 	$.save_clientcity_button.show();
- });
-
-	$.save_clientstate_button.addEventListener('click', function(_e) {
-    $.clientstate_tf.blur();
-    var clientstate = $.clientstate_tf.value;
-    Ti.API.info("clientstate entered is: "+clientstate);
-    Titanium.App.Properties.setString('clientstate',clientstate);
-    Ti.API.info("clientstate obtained is: "+Titanium.App.Properties.getString('clientstate',"none"));
-    $.save_clientstate_button.hide();
- });
-
-	$.clientstate_tf.addEventListener("focus", function(){
- 	$.save_clientstate_button.show();
- });
-
-	$.save_clientproject_button.addEventListener('click', function(_e) {
-    $.clientproject_tf.blur();
-    var clientproject = $.clientproject_tf.value;
-    Ti.API.info("clientproject entered is: "+clientproject);
-    Titanium.App.Properties.setString('clientproject',clientproject);
-    Ti.API.info("clientproject obtained is: "+Titanium.App.Properties.getString('clientproject',"none"));
-    $.save_clientproject_button.hide();
- });
-
-	$.clientproject_tf.addEventListener("focus", function(){
- 	$.save_clientproject_button.show();
+	// Adding row to the table view
+	$.enterclient_table.insertRowAfter(count,newRow);
+	var count = count+1;
+	console.log("new count :" +count);
+	Titanium.App.Properties.setInt('count',count);
+	
+	itemTextField.addEventListener('blur', function(_e) {
+ 	var clientclient = itemTextField.value;
+ 	Ti.API.info("clientclient entered in dyn field is: "+clientclient);
+ 	console.log("e JSON of textfield: "+JSON.stringify(_e));
  });
  
- 	$.save_clientcompany_button.addEventListener('click', function(_e) {
-    $.clientcompany_tf.blur();
-    var clientcompany = $.clientcompany_tf.value;
-    Ti.API.info("clientcompany entered is: "+clientcompany);
-    Titanium.App.Properties.setString('clientcompany',clientcompany);
-    Ti.API.info("clientcompany obtained is: "+Titanium.App.Properties.getString('clientcompany',"none"));
-    $.save_clientcompany_button.hide();
- });
+	//var textfield = Ti.UI.createTextField({keyboardType: Ti.UI.KEYBOARD_NUMBER_PAD, returnKeyType: Ti.UI.RETURNKEY_DONE, backgroundColor: '#262626', border: 1, width: 100});
 
-	$.clientcompany_tf.addEventListener("focus", function(){
- 	$.save_clientcompany_button.show();
- });
+}
 
- 	$.submit_button.addEventListener("click", function(){
-		
- 	var now = new Date();
- 	var clientlastname = Titanium.App.Properties.getString('clientlastname',"none");
- 	var clientfirstname = Titanium.App.Properties.getString('clientfirstname',"none");
- 	var clientphone = Titanium.App.Properties.getString('clientphone',"none");
- 	var clientemail = Titanium.App.Properties.getString('clientemail',"none");
- 	var clientstreetaddress = Titanium.App.Properties.getString('clientstreetaddress',"none");
- 	var clientcity = Titanium.App.Properties.getString('clientcity',"none");
- 	var clientstate = Titanium.App.Properties.getString('clientstate',"none");
- 	var clientproject = Titanium.App.Properties.getString('clientproject',"none");
- 	var clientcompany = Titanium.App.Properties.getString('clientcompany',"none");
- 	alert("On "+now+" : Info on: "+clientfirstname+" "+clientlastname+" with "+clientphone+" and email "+clientemail+" at "+clientstreetaddress+", "+clientcity+", "+clientstate+". submitted");
- 	var fcsv = Ti.Filesystem.getFile(Ti.Filesystem.tempDirectory,'enterclient.csv');
- 	var ftxt = Ti.Filesystem.getFile(Ti.Filesystem.tempDirectory,'enterclient.txt');
-	fcsv.write(now+", "+clientfirstname+", "+clientlastname+", "+clientphone+", "+clientemail+", "+clientstreetaddress+", "+clientcity+", "+clientstate+'\n', true); // write to the file
-	ftxt.write(now+", "+clientfirstname+", "+clientlastname+", "+clientphone+", "+clientemail+", "+clientstreetaddress+", "+clientcity+", "+clientstate+'\n', true); // write to the file
+$.lineitem_tf.addEventListener('blur', function(_e) {
+    var clientclient = $.lineitem_tf.value;
+    Ti.API.info("clientclient entered is: "+clientclient);
+    Titanium.App.Properties.setString('clientclient',clientclient);
+    Ti.API.info("clientclient obtained is: "+Titanium.App.Properties.getString('clientclient',"none"));
+    console.log("e JSON of textfield: "+JSON.stringify(_e));
+ });
+ 
+ var itemvalue =[];
+
+ function saveHandler(){
+ 	console.log("saving all data ");
+ 	var tabledata = [];	
+ 	var getvalue = ["clientfirstname","clientlastname","clientphone","clientemail","clientstreetaddress","clientcity","clientstate","clientcompany"];
+ 	for (i=0;i<$.enterclient_table.data[0].rowCount;i++) {		
+ 		console.log("children count : "	+$.enterclient_table.data[0].rows[i].children.length);
+ 		for (j=0;j<+$.enterclient_table.data[0].rows[i].children.length;j++) { 			
+			console.log("JSON stringify table 0 row "+i+' : ' +JSON.stringify($.enterclient_table.data[0].rows[i]));
+			console.log("JSON stringify table 0 row "+i+'w/children '+j+' : ' +JSON.stringify($.enterclient_table.data[0].rows[i].children[j]));
+			tabledata.push({data1:$.enterclient_table.data[0].rows[i].children[j].id || "none",data2:$.enterclient_table.data[0].rows[i].children[j].value || "none"});
+			for (z=0;z<getvalue.length;z++){
+				var subject = getvalue[z];
+				if ( $.enterclient_table.data[0].rows[i].children[j].id == subject+"_tf") {					
+					 	eval("var "+subject+" = $.enterclient_table.data[0].rows[i].children[j].value || $.enterclient_table.data[0].rows[i].children[j].text;");		 
+				};
+			}		
+		};
+	};
+	console.log("tabledata are: "+JSON.stringify(tabledata));
+	console.log("enterclient.js::saveHandler:: detect array dyn variable: "+clientfirstname+","+clientlastname+","+clientphone+","+clientemail+","+clientstreetaddress
+	+","+clientcity+","+clientstate+","+clientcompany);
+	//once tabledata is populated, find submission value
+	var clientname = [];
+	var clientdescr = [];
+	var lineitem = [];
+	var lineitemqty = [];
+	var lineitemprice = [];
+	for (i=0;i<tabledata.length;i++){
+		if (tabledata[i].data1 == "clientname_tf") {  clientname.push({ name:tabledata[i].data2 }); };
+		if (tabledata[i].data1 == "clientdescr_tf") {  clientdescr.push({ descr:tabledata[i].data2 }); };
+		if (tabledata[i].data1 == "lineitem_tf") {  lineitem.push({ item:tabledata[i].data2}); };
+		if (tabledata[i].data1 == "lineitemqty_tf") {  lineitemqty.push({ itemqty:tabledata[i].data2 }); };
+		if (tabledata[i].data1 == "lineitemprice_tf") {  lineitemprice.push({ itemprice:tabledata[i].data2 }); };
+	}
+	console.log("lineitem: "+JSON.stringify(lineitem));
+	console.log("lineitemqty: "+JSON.stringify(lineitemqty));
+	console.log("lineitemprice: "+JSON.stringify(lineitemprice));
+	var item = [];
+	item.push({'descr':clientdescr[0].descr});
+	for (i=0;i<lineitem.length;i++){
+		item.push({
+			'lineitem':lineitem[i].item,
+			'qty':lineitemqty[i].itemqty,
+			'price':lineitemprice[i].itemprice
+		});
+	}
+	console.log("enterclient::saveHandler:JSON.stringify(item): "+JSON.stringify(item));
+	var clientnumber = "10";
+	var name = clientfirstname+' '+clientlastname;
+	var customerno = "2";
+	var total = "200";
+	var bal = "200";
+	var paid = "50%";
+	var lastpaiddate = "4/1/2015";
+	var followupdate = "5/1/2015";
+	var duedate = "6/1/2015";
+	var currency = "USD";
+	var status = "owed";
+	var country = "USA";
+	var clientclient = clientname[0].name;
+	var notes = JSON.stringify(item).toString().replace(/:/g,'cOlOn');
+	console.log("enterclient.js::saveHandler::clientclient: "+clientclient+" clientfirstname: "+clientfirstname+" clientlastname "+clientlastname);	
+	submit(clientclient,clientfirstname,clientlastname,clientcompany,clientphone,clientemail,clientstreetaddress,clientcity,clientstate,country,"pending",notes,"0","6/1/2015","7/1/2015");
+	///console.log('submit('+clientnumber+','+name+','+customerno+','+total+','+bal+','+paid+','+lastpaiddate+','+followupdate+','+clientphone+','+clientemail+','+duedate+','
+	///+currency+','+status+')');
+ }; 
+ 
+ function submit(clientclient,clientfirstname,clientlastname,clientcompany,clientphone,clientemail,clientstreetaddress,clientcity,clientstate,country,status,notes,percentcompletion,nextappt,datedue) {	
+ 	var now = Date.now();
 	var xmldatastring = '<entry xmlns=\'http://www.w3.org/2005/Atom\' xmlns:gsx=\'http://schemas.google.com/spreadsheets/2006/extended\'>'
-	+'<gsx:col1>'+clientfirstname+'</gsx:col1><gsx:col2>'+clientfirstname+'</gsx:col2><gsx:col3>'
+	+'<gsx:col1>'+now+'</gsx:col1><gsx:col2>'+clientfirstname+'</gsx:col2><gsx:col3>'
 	+clientlastname+'</gsx:col3><gsx:col4>'+clientcompany+'</gsx:col4><gsx:col5>'
 	+clientphone+'</gsx:col5><gsx:col6>'+clientemail+'</gsx:col6><gsx:col7>'+clientstreetaddress+'</gsx:col7><gsx:col8>'+clientcity+'</gsx:col8><gsx:col9>'+clientstate
-	+'</gsx:col9><gsx:col10>'+'USA'+'</gsx:col10><gsx:col11>'+'NA'+'</gsx:col11><gsx:col12>NA</gsx:col12><gsx:col13>NA</gsx:col13><gsx:col14>NA</gsx:col14><gsx:col15>NA</gsx:col15><gsx:col16>NA</gsx:col16></entry>';
+	+'</gsx:col9><gsx:col10>'+country+'</gsx:col10><gsx:col11>NA</gsx:col11><gsx:col12>NA</gsx:col12><gsx:col13>NA</gsx:col13><gsx:col14>'+now+'</gsx:col14><gsx:col15>NA'
+	+'</gsx:col15><gsx:col16>'+now+'</gsx:col16></entry>';
 	Ti.API.info('xmldatastring to POST: '+xmldatastring);
 	var xhr =  Titanium.Network.createHTTPClient({
     onload: function() {
@@ -156,26 +232,71 @@ exports.openMainWindow = function(_tab) {
     },
     onerror: function(e) {
     	Ti.API.info("error e: "+JSON.stringify(e));
-        alert("Unable to communicate to the cloud. Please try again"); 
+        alert("Unable to communicate to the cloud. Please try again."); 
     }
 });
-	xhr.open("POST", 'https://spreadsheets.google.com/feeds/list/1ECkNoyzgeSu8WkVs3kBnlY8MjJRIAc787nVs6IJsA9w/od6/private/full');
+    //var spreadsheet_id = '1-Wz7Apn4AvVpfqcNyMgfqyKA8OAoLNy5Bl0d_jQ9IZk';
+    var spreadsheet_id = Titanium.App.Properties.getString('client');
+	xhr.open("POST", 'https://spreadsheets.google.com/feeds/list/'+spreadsheet_id+'/od6/private/full');
 	xhr.setRequestHeader("Content-type", "application/atom+xml");
 	xhr.setRequestHeader("Authorization", 'Bearer '+ googleAuth.getAccessToken());
 	xhr.send(xmldatastring);
 	Ti.API.info('done POSTed');
 
+ }
+ 
+ $.enterclient_window.addEventListener('click',function(e){
+	 	$.lineitem_tf.blur();
+	 	$.lineitemqty_tf.blur();
+	 	$.lineitemprice_tf.blur();
+	 });
+	 
+ $.clientdetail_row.addEventListener('click',function(e){
+	 	allTFBlur();
+	 });
+ 
+  function allTFBlur(){
+ 		$.lineitem_tf.blur();
+	 	$.lineitemqty_tf.blur();
+	 	$.lineitemprice_tf.blur();
+ };
+ 
+var addnewclientrow = [ $.existing, $.clientdetail_row, $.itemline_row, $.itemdetail_row, $.addrow_row, $.itemlineend_row,$.totalrow ];
 
- });
+$.enterclient_table.setData(addnewclientrow)	;
 
-};
+$.check_client.addEventListener('click', function(e){
+	var clientController = Alloy.createController('client',{
+			sourcecall: 'enterclient'
+		});
+	clientController.openMainWindow($.enterclient_tab);
+	
+});
 
+var scope = ['https://spreadsheets.google.com/feeds', 'https://docs.google.com/feeds','https://www.googleapis.com/auth/calendar','https://www.googleapis.com/auth/calendar.readonly','https://www.googleapis.com/auth/drive'];
+scope.push ("https://www.googleapis.com/auth/drive.appdata");
+scope.push ("https://www.googleapis.com/auth/drive.apps.readonly");
+scope.push ("https://www.googleapis.com/auth/drive.file");
 var GoogleAuth = require('googleAuth');
 var googleAuth = new GoogleAuth({
 	clientId : '306793301753-8ej6duert04ksb3abjutpie916l8hcc7.apps.googleusercontent.com',
 	clientSecret : 'fjrsVudiK3ClrOKWxO5QvXYL',
 	propertyName : 'googleToken',
-	scope : ['https://www.googleapis.com/auth/tasks', 'https://www.googleapis.com/auth/tasks.readonly'],
+	scope : scope,
 	quiet: false
 	//scope : ['https://spreadsheets.google.com/feeds', 'https://docs.google.com/feeds'],
 });
+
+
+
+$.enterclient_table.addEventListener('click', function(e){
+	console.log("JSON stringify after table row is clicked : " +JSON.stringify(e));
+		 	$.lineitem_tf.blur();
+	 	$.lineitemqty_tf.blur();
+	 	$.lineitemprice_tf.blur();
+});
+ 
+
+
+
+ 
